f929601af3949fcb7fe678a8c5c6bdc1bfed4f84
AR123456 <coloradoz99@gmail.com>
2021-08-23 06:01:45 -0600

    JS jump function

--- app.js
+++ app.js
@@ -56,6 +56,11 @@ function startGame() {
 
 function jump() {
   //controle velocity
+  if (player.v === 0) {
+    return false;
+  }
+  // ////////////////// the bigger this number the greater the velocity
+  player.v = 3.2;
 }
 function render() {
   ////////////////////////////main gamin function
@@ -63,19 +68,65 @@ function render() {
   const thisTime = performance.now();
   //   delta time
   const dt = Math.min(32, Math.max(8, thisTime - lastTime)) / 16.666;
-  lastTime = thistime;
+  lastTime = thisTime;
   // render buildings
-  if (nextBuidingX < gameProgress + 960 + speed * dt) {
+  if (nextBuildingX < gameProgress + 960 + speed * dt) {
     createBuildings();
   }
+  //  https://www.youtube.com/watch?v=atxvy-FVz4Y
+  let base = 0;
+  const destroyBuildings = [];
+  let nextBuilding = buildings[0];
+
+  buildings.forEach((building, ix) => {
+    if (building.x < player.x) {
+      base = building.height;
+      nextBuilding = buildings[ix + 1];
+    }
+    if (building.x < gameProgress + 180) {
+      destroyBuildings.push(ix);
+    }
+    building.div.style.setProperty(
+      "--building-x",
+      building.x - gameProgress + "px"
+    );
+  });
+
   // render player
+  if (player.v > 0) {
+    player.y += player.v;
+    player.v = Math.max(0, player.v - g);
+  } else if (base < player.y) {
+    // player is falling down here
+    player.y = Math.max(base, player.y + player.v);
+    player.v -= g;
+    //Thttps://www.youtube.com/watch?v=atxvy-FVz4Y
+  } else {
+    player.v = 0;
+  }
+  playerDiv.classList = `player ${player.v === 0 ? "run" : "jump"}`;
+  let nextPlayerX = player.x + speed;
+  if (nextPlayerX - gameProgress < 720) {
+    nextPlayerX += 1 / speed;
+  }
+  if (nextPlayerX > nextBuilding.x && nextBuilding.height > player.y) {
+    nextPlayerX = nextBuilding.x;
+  }
+  player.x = nextPlayerX;
+  playerDiv.style.setProperty("--player-x", nextPlayerX - gameProgress + "px");
+  playerDiv.style.setProperty("--player-y", 320 - player.y + "px");
+  // TODO restart here https://www.youtube.com/watch?v=atxvy-FVz4Y
   // render road
   // render distroy buildings
-  // render set progress and re render
+  // /////////render set progress and re render
   // increase speed
-  speed += 0.001 * dt;
-  // game progress set to speed
-  gameProgress += speed * dt;
+  // speed += 0.001 * dt;
+  // // game progress set to speed
+  // gameProgress += speed * dt;
+  // console.log({ dt });
+  speed += 0.001;
+  gameProgress += speed;
+
   if (gameStatus === "on") {
     requestAnimationFrame(render);
   }
@@ -94,7 +145,7 @@ function createBuildings() {
   const buildingDiv = document.createElement("div");
   buildingDiv.classList = "building";
   buildingDiv.style.width = building.width + "px";
-  buildingDiv.style.height = buiding.height + "px";
+  buildingDiv.style.height = building.height + "px";
   // style will need hue
   buildingDiv.style.setProperty("--hue", Math.random() * 360 + "deg");
   // https://www.youtube.com/watch?v=atxvy-FVz4Y
